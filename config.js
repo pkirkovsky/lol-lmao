fa43227d07b3c71fadd5b490eb96951a0acd360b
Carlos Wunsche <wunschecarlos@gmail.com>
2023-03-02 22:30:10 -0500

    Squid

--- config.js
+++ config.js
@@ -1,14 +1,26 @@
-/**
- * Index for both day and month are base-1 indexes
- * I.e
- * Month of January = 1
- * First day of the month = 1
- *
- * Year should be a 4 digit number
- * I.e
- * 2023
- */
+/* ------------------------------------------------- */
+/*                    INSTRUCTIONS                   */
+/* ------------------------------------------------- */
 
-exports.INIT_MONTH = 1;
-exports.INIT_DAY = 3;
+/* ---------------------- DATE --------------------- */
+// E.g:
+// Month of January = 1
+// Third day of the month = 3
+//
+// Year should be a 4 digit number, i.e 2023
+
+exports.INIT_MONTH = 4;
+exports.INIT_DAY = 4;
 exports.INIT_YEAR = 2021;
+
+/* ------------------ ALIEN TYPE ------------------- */
+// Choose between: squid - crab - octopus
+
+exports.ALIEN_TYPE = 'squid';
+
+/* ------------------- BRIGHTNESS ------------------ */
+// 1 = Most bright
+// 2 = Medium brightness
+// 3 = Darkest
+
+exports.BRIGHTNESS = 2;
--- index.js
+++ index.js
@@ -1,47 +1,59 @@
 /* --------------------- CONFIG -------------------- */
-/**
- * Open config.js and fill the placeholders with the
- * date you want to begin drawing!
- */
+//
+// Open config.js and replace the placeholders
+//
 
 /* -------------------- MODULES -------------------- */
-const fs = require('fs');
-const { crab } = require('./aliens.js');
-const { INIT_MONTH, INIT_DAY, INIT_YEAR } = require('./config.js');
+const fileSystem = require('fs');
+const aliens = require('./aliens');
+const {
+  INIT_MONTH,
+  INIT_DAY,
+  INIT_YEAR,
+  ALIEN_TYPE,
+  BRIGHTNESS,
+} = require('./config');
 
 /* ------------------- FUNCTIONS ------------------- */
-const getContentString = function (initialDate, alien) {
-  let string = '';
+const getContentString = function (initialDate, alien, brightness = 1) {
+  const oneDay = 1;
   let date = initialDate;
-
+  let output = '';
   alien.pixels.forEach(pixel => {
-    date = new Date(date.setDate(date.getDate() + 1));
+    date = new Date(date.setDate(date.getDate() + oneDay));
     if (!pixel) return;
 
-    string += `echo 'A commit was made on this date: ${date.toDateString()}' >> commit-history.md \n`;
-    string += `git add commit-history.md\n`;
-    string += `git commit --date="${date.toDateString()}" -m "${
-      alien.name
-    }" \n`;
+    for (let i = 1; i < brightness + 1; i++) {
+      output += `echo 'A commit was made on this date: ${date.toDateString()}' >> commit-history.md \n`;
+      output += `git add commit-history.md\n`;
+      output += `git commit --date="${date.toDateString()}" -m "${
+        alien.name
+      }" \n`;
+    }
   });
-  return string;
+  return output;
 };
 
 /* ----------------- CREATE SCRIPT ----------------- */
 const initialDate = new Date(INIT_YEAR, INIT_MONTH - 1, INIT_DAY - 1);
 
 // Generate script contents string
-const scriptContents = getContentString(initialDate, crab);
+const scriptContents = getContentString(
+  initialDate,
+  aliens[ALIEN_TYPE],
+  BRIGHTNESS
+);
 
 // Clear markdown commit history file
-fs.writeFileSync(`${__dirname}/commit-history.md`, '', 'utf8');
+fileSystem.writeFileSync(`${__dirname}/commit-history.md`, '', 'utf8');
 
 // Write contents to bash script file
-fs.writeFileSync(`${__dirname}/script.sh`, scriptContents, 'utf8');
+fileSystem.writeFileSync(`${__dirname}/script.sh`, scriptContents, 'utf8');
 
 /* ------------------- LET'S GO! ------------------- */
-/**
- * Run script.sh using bash or git bash terminal
- * like this: ./script.sh
- * and enjoy!
- */
+//
+// 1) Open a bash or Git bash terminal
+// 2) Run script like this:  ./script.sh
+// 3) Sync changes with origin repo
+// 4) Enjoy!
+//
