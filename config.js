5c02010d31275a97accea70c40b965bac73cc7b4
Carlos Wunsche <wunschecarlos@gmail.com>
2023-03-02 20:42:39 -0500

    beta released

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Index for both day and month are base-1 indexes
+ * I.e
+ * Month of January = 1
+ * First day of the month = 1
+ *
+ * Year should be a 4 digit number
+ * I.e
+ * 2023
+ */
+
+exports.INIT_DATE_MONTH = 1;
+exports.INIT_DATE_DAY = 3;
+exports.INIT_DATE_YEAR = 2021;
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const { crab } = require('./aliens.js');
 const fs = require('fs');
+const { crab } = require('./aliens.js');
+const {
+  INIT_DATE_MONTH,
+  INIT_DATE_DAY,
+  INIT_DATE_YEAR,
+} = require('./config.js');
 
-console.log(crab.pixels);
+const initialDate = new Date(
+  INIT_DATE_YEAR,
+  INIT_DATE_MONTH - 1,
+  INIT_DATE_DAY - 1
+);
 
-// fs.writeFile(`${__dirname}/script.sh`, 'hello', 'utf8');
+const scriptContents = getContentString(initialDate, crab);
+
+fs.writeFileSync(`${__dirname}/script.sh`, scriptContents, 'utf8');
+
+function getContentString(dateObj, alien) {
+  let string = '';
+  let date = dateObj;
+
+  alien.pixels.forEach(el => {
+    date = new Date(date.setDate(date.getDate() + 1));
+    if (!el) return;
+
+    string += `git commit --date="${date.toDateString()}" -m "Commit Invaders: ${
+      alien.name
+    }" \n`;
+  });
+  return string;
+}
