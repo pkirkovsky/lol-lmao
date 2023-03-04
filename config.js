1a56a09f46702af8c639d23b8bb90cd45c3b67ff
Carlos Wunsche <wunschecarlos@gmail.com>
2023-03-04 17:43:20 -0500

    placeholders

--- config.js
+++ config.js
@@ -9,9 +9,9 @@
 //
 // Year should be a 4 digit number, i.e 2023
 
-exports.INIT_MONTH = 7;
-exports.INIT_DAY = 31;
-exports.INIT_YEAR = 2022;
+exports.INIT_MONTH = 1;
+exports.INIT_DAY = 3;
+exports.INIT_YEAR = 2021;
 
 /* ------------------ ALIEN TYPE ------------------- */
 // Choose between: squid - crab - octopus
